fa0513f53380023702faabf72e29d06eb924ec40
Yishu Xue <yishu.xue@uconn.edu>
2019-04-28 12:44:59 -0400

    Update simnull.R

--- simulation/simnull.R
+++ simulation/simnull.R
@@ -64,8 +64,6 @@ sim <- function(k){
                 loglike2 <- sum(rev(delta) * 
                                 (X %*% locfit$coefficients -
                                 log(cumsum(exp(X %*% locfit$coefficients)))))
-                aiclocal[j] <- aiclocal[j] - 2 * loglike2
-                
                 p1locmat[i, j] <- locfit$coefficients[1]
                 p2locmat[i, j] <- locfit$coefficients[2]
                 p3locmat[i, j] <- locfit$coefficients[3]
